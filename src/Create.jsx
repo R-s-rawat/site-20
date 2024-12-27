f8f03948afe9d3368713cf9b0de7d57f25888515
R-s-rawat <rohitrawat590590@gmail.com>
2024-12-27 08:11:17 +0530

    minor change

--- src/Create.jsx
+++ src/Create.jsx
@@ -5,7 +5,7 @@ const Create = () => {
    <>
    <div className='d-flex w-100 vh-100 justify-content-center align-items-center'>
     <div className='w-50 border bg-secondary text-white p-5'>
-      <h3>Add</h3>
+      <h3>Add New User</h3>
       <form>
         <div>
           <label htmlFor='name'>Name:</label>
