bb05e80e14935a1ebfdd92474bfc802c61ee45e0
R-s-rawat <rohitrawat590590@gmail.com>
2024-12-27 08:07:47 +0530

    added ui create

--- src/Create.jsx
+++ src/Create.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Create = () => {
   return (
-    <div>Create</div>
+   <>
+   <div className='d-flex w-100 vh-100 justify-content-center align-items-center'>
+    <div className='w-50 border bg-secondary text-white p-5'>
+      <h3>Add</h3>
+      <form>
+        <div>
+          <label htmlFor='name'>Name:</label>
+          <input type='text' name='name' className='form-control'/>
+        </div>
+        <div>
+          <label htmlFor='name'>Email:</label>
+          <input type='email' name='email' className='form-control'/>
+        </div>
+        <br/>
+        <button className='btn btn-info'>Submit</button>
+      </form>
+    </div>
+   </div>
+   </>
   )
 }
 
